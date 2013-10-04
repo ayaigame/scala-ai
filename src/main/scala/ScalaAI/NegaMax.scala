3c0de77455267e5fe700e2aed312d02b8de2b2ec
Jarrad Battaglia <jarradbattaglia@Jarrads-Mac.local>
2013-10-04 18:25:40 -0400

    added negamax function and implementation

--- src/main/scala/ScalaAI/NegaMax.scala
+++ src/main/scala/ScalaAI/NegaMax.scala
@@ -1,8 +1,31 @@
 package ScalaAI
 
+import scala.collection.mutable.ArrayBuffer
+
+
 object NegaMax {
+
+  def negamax(node: Node, depth: Double, color: Double):Double = {
+    //if depth is 0 or node is terminal (no children), then return color times node data
+    if(depth == 0 || node.children.length == 0) {
+      //println("ChildVal of depth: " + (depth) + " is " + (color*node.data))
+      return color * node.data
+    }
+    var bestvalue:Double = Double.NegativeInfinity
+    var childNodes:ArrayBuffer[Node] = node.children
+    for (child <- childNodes) {
+    	var childval:Double = -negamax(child, depth-1, color)
+    	//println("Bestval is " + bestvalue)
+    	bestvalue = math.max(bestvalue, childval)
+    }
+    
+    return bestvalue
+  }
+  
   def run(root: Node):Double = {
-    Console.println("Running Negamax")
-    return 0.0
+    println("Running Negamax")
+    var nega: Double = negamax(root, 5, 1)
+    println("The NegaMax value is: " + nega)
+    return nega
   }
 }
