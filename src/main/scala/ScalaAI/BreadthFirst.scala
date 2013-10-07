7457979c2a1390944bfb8ef36c52ae099006befa
Kurt Wheeler <kgw26@cs.drexel.edu>
2013-10-07 11:38:08 -0400

    Implements breadth first search.

--- src/main/scala/ScalaAI/BreadthFirst.scala
+++ src/main/scala/ScalaAI/BreadthFirst.scala
@@ -3,6 +3,32 @@ package ScalaAI
 object BreadthFirst {
   def run(root: Node, goal: Int):Int = {
     Console.println("Running BreadthFirst")
+    if(root.data == goal) {
+      Console.println("Found it at root!")
+      return root.data
+    }
+    else {
+      return searchNode(root, goal)
+    }
+  }
+
+  def searchNode(node: Node, goal: Int):Int = {
+   Console.println("Checking " + node.data + "'s children:") 
+   for(child <- node.children) {
+      Console.print(child.data + " ")
+      if(child.data == goal) {
+       Console.println("\nFound it!")
+       return child.data
+      }
+    }
+
+    Console.println("")
+
+		for(child <- node.children) {
+      val value = searchNode(child, goal)
+      if(value > -1) return value
+    }
+
     return -1
   }
 }
