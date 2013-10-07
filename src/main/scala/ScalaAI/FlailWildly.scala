cbc24bce71b0de8ee1ef53577741d1b7ac541ebc
Joshua Henry <henry41043@gmail.com>
2013-10-07 19:59:56 -0400

    Moved defs outside of run() and cleaned up comments

--- src/main/scala/ScalaAI/FlailWildly.scala
+++ src/main/scala/ScalaAI/FlailWildly.scala
@@ -2,36 +2,30 @@ package ScalaAI
 import java.util.Random
 
 object FlailWildly {
-  def run(s1: String, s2: String):String = {
-    Console.println("\n\nRunning FlailWildly")
 
-    // a rule is (index, char)
-    // so for the word "beach" the rule (0,p) will result in "peach"
-
-    var currentState = s1
-    var endState = s2
     var alphabet = List[Char]('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
     val source = scala.io.Source.fromFile("src/test/resources/fiveLetterWords.txt")
     val lines = source.mkString
     var words = lines.split("\n")
     var k = 0
+    
     for( k <- 0 until words.length) {
         words(k) = words(k).trim()
     }
 
     source.close()
 
-    def goal(state: String):Boolean = {
-    	if(state == endState){
-    		return true
-    	}else{
-    		return false
-    	}	
+    def goal(state: String, endState: String):Boolean = {
+        if(state == endState){
+            return true
+        }else{
+            return false
+        }   
     }
 
     def applyRule(rule: (Int, Char), state: String):String = {
-    	var tempArray = state.toCharArray()
-    	tempArray(rule._1) = rule._2
+        var tempArray = state.toCharArray()
+        tempArray(rule._1) = rule._2
         var newState = ""
         for(i <- tempArray) {
             newState = newState + i.toString()
@@ -79,30 +73,26 @@ object FlailWildly {
         Console.println("Change letter in position " + rule._1 + " to " + rule._2)
     }
 
-    var count = 0
+    def run(startState: String, endState: String):String = {
+        Console.println("\n\nRunning FlailWildly")
+        Console.println("Start State: " + startState)
 
-    Console.println("Start State: " + currentState)
+        var currentState = startState
+        var count = 0
 
-    while(!goal(currentState)){
-        var possibleChoices = generateRules(currentState)
-        //describeState(currentState)
-        //Console.println("Possible Rules: ")
-        for( i <- possibleChoices) {
-            //describeRule(i)
+        while(!goal(currentState, endState)){
+            var possibleChoices = generateRules(currentState)
+            var rand = new Random(System.currentTimeMillis())
+            var random_index = rand.nextInt(possibleChoices.length)
+            var result = possibleChoices(random_index)
+
+            currentState = applyRule(result, currentState)
+            count = count + 1
         }
-        var rand = new Random(System.currentTimeMillis())
-        var random_index = rand.nextInt(possibleChoices.length)
-        var result = possibleChoices(random_index)
-        //Console.println("Rule picked: ")
-        //describeRule(result)
-        currentState = applyRule(result, currentState)
-        //Console.println("\n")
-        count = count + 1
+
+        Console.println("Final State: " + currentState)
+        Console.println("FlailWildly finished after " + count + " rules where applied\n\n")
+
+        return currentState
     }
-
-    Console.println("Final State: " + currentState)
-    Console.println("FlailWildly finished after " + count + " rules where applied\n\n")
-
-    return currentState
-  }
 }
