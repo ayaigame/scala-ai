7308a71450ecd5bd0f06db846438b77ef63aff01
Ryan Lerch <lerchr@gmail.com>
2013-10-08 16:00:32 -0400

    Depth first search

--- src/main/scala/ScalaAI/DepthFirst.scala
+++ src/main/scala/ScalaAI/DepthFirst.scala
@@ -1,8 +1,25 @@
 package ScalaAI
 
 object DepthFirst {
-  def run(root: Node, goal: Int): Int = {
-    Console.println("Running DepthFirst")
+  def searchNode(node: Node, goal: Int):Int = {
+    if(node.data == goal) {
+      Console.println("\nFound it!")
+      return node.data
+    }
+
+    Console.println("Checking " + node.data)
+
+
+    for(child <- node.children) {
+      val value = searchNode(child, goal)
+      if(value > -1) return value
+    }
+
     return -1
+  }
+
+  def run(root: Node, goal: Int):Int = {
+    Console.println("Running Depthfirst search")
+    return searchNode(root, goal)
   }
 }
